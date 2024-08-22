9a561d9a03260c2fad30533609efe73be1edd648
1002Ayushi <117763804+1002Ayushi@users.noreply.github.com>
2024-08-22 17:14:06 +0530

    weekDay_code

---
+++ Logic Building Session/Assignments/Assignment2/weekDayCode.java
@@ -0,0 +1,35 @@
+class Days{
+public static void main(String[] args){
+int day = 3;
+switch(day){
+    case 1:
+      System.out.println("Monday");
+      System.out.println("weekday");
+      break;
+    case 2:
+      System.out.println("Tuesday");
+      System.out.println("weekday");
+      break;
+    case 3:
+      System.out.println("Wednesday");
+      System.out.println("weekday");
+      break;
+    case 4:
+      System.out.println("Thursday");
+      System.out.println("weekday");
+      break;
+    case 5:
+      System.out.println("Friday");
+      System.out.println("weekday");
+      break;
+    case 6:
+      System.out.println("Saturday");
+      System.out.println("weekday");
+      break;
+    case 7:
+      System.out.println("Sunday");
+      System.out.println("weekend");
+      break;
+}
+}
+}
